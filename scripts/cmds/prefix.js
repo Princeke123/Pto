b42891575cc3a60929b74b1507efece0f60f22b3
Princeke123 <160714583+Princeke123@users.noreply.github.com>
2026-09-11 23:51:29 +0300

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -93,7 +93,7 @@ module.exports = {
     const time = moment().tz("Asia/Dhaka").format("hh:mm A");
     const date = moment().tz("Asia/Dhaka").format("DD MMM YYYY");
 
-    const owner = global.GoatBot.config.adminName || "MAMUN";
+    const owner = global.GoatBot.config.adminName || "NCS PRO";
 
     return message.reply(
 `╭━━━〔 PREFIX 〕━━━╮
