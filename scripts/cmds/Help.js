0f6a63ba5de2ef6370792bd4f7d40088bd942ff4
Princeke123 <160714583+Princeke123@users.noreply.github.com>
2026-09-11 23:48:45 +0300

    Update Help.js

--- scripts/cmds/Help.js
+++ scripts/cmds/Help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: 𝐌𝐀𝐌𝐔𝐍 彡\n├‣ WHATSAPP\n╰‣ 01830981279`;
+                        msg += `\n╭─✦ ADMIN: NCS KIPE 彡\n├‣ WHATSAPP\n╰‣ 0705904250`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
